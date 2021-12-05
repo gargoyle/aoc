6a9065d5ee1c43db2b3dc4fc9fd1a86cb7b283cb
Paul Court <paul.court@flxs.co.uk>
2021-12-05 14:42:03 +0000

    Day 5

---
+++ Day 5/day5.php
@@ -0,0 +1,34 @@
+<?php
+
+spl_autoload_register(function ($className) {
+    $file = $className . '.php';
+    if(file_exists($file)) {
+        require_once $file;
+    }
+});
+
+$begin = microtime(true);
+
+$inputData = file('input.txt');
+$intersects = [];
+$dangerPoints = [];
+
+foreach ($inputData as $raw) {
+    foreach (Line::fromVentData($raw)->points() as list($x, $y)) {
+        if (!isset($intersects[$x][$y])) {
+            $intersects[$x][$y] = 0;
+        }
+        $intersects[$x][$y]++;
+        
+        if ($intersects[$x][$y] >= 2) {
+            $dangerPoints[] = $x.':'.$y;
+        }
+    }
+}
+
+$dangerPoints = array_unique($dangerPoints);
+echo "Danger points: " . count($dangerPoints) . "\n";
+
+printf("Runtime = %f seconds\n", 
+        (microtime(true) - $begin)
+        );

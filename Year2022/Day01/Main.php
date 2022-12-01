e0e64b0f959650761e7dfd043d87dbe8902bd40f
Paul Court <emails@paulcourt.co.uk>
2022-12-01 20:51:50 +0000

    2022, Day 1: Final Solution

--- Year2022/Day01/Main.php
+++ Year2022/Day01/Main.php
@@ -9,13 +9,52 @@ class Main
         return "Calorie Counting";
     }
     
+    private static function cleanedLines(): array
+    {
+        $filename = __DIR__ . DIRECTORY_SEPARATOR . (TEST_MODE ? "test_" : "") . "input.txt";
+        $lines = file($filename);
+        array_walk($lines, function (&$v) {
+            $v = trim($v);
+        });
+        return $lines;
+    }
+    
+    private static function loadElfCalories(): array
+    {
+        $lines = self::cleanedLines();
+        
+        $elfCalories = [];
+        $elfNum = 1;
+        $currentCals = 0;
+        
+        for ($i = 0; $i < count($lines); $i++) {
+            $currentCals += (int) $lines[$i];
+            
+            if ($lines[$i] == "") {
+                $elfCalories[$elfNum] = $currentCals;
+                $currentCals = 0;
+                $elfNum++;
+            }
+        }
+        $elfCalories[$elfNum] = $currentCals;
+        
+        return $elfCalories;
+    }
+    
     public static function TaskOne(): string
     {
-        return "Task 1";
+        $elfCalories = self::loadElfCalories();
+        asort($elfCalories);
+
+        return array_pop($elfCalories);
     }
     
     public static function TaskTwo(): string
     {
-        return "Task 2";
+        $elfCalories = self::loadElfCalories();
+        arsort($elfCalories);
+        $topThree = array_slice($elfCalories, 0, 3);
+        
+        return array_sum($topThree);
     }
 }

1ba40d26729341ad45ea6d0ab42cee6623007342
Paul Court <emails@paulcourt.co.uk>
2022-12-01 20:18:26 +0000

    2022 code re-organise

--- Year2021/aoc.php
+++ Year2021/aoc.php
@@ -18,20 +18,22 @@ define('START_TIME', microtime(true));
 $opts = getopt("t");
 define('TEST_MODE', isset($opts['t']));
 define('DAY', $argv[TEST_MODE ? 2 : 1] ?? 1);
-define('YEAR', 2022);
 
-//$filename = sprintf("Year%04d/Day%02d/", YEAR, DAY);
-//$filename .= (TEST_MODE ? "test_" : "") . "input.txt";
-//$lines = file($filename);
-//array_walk($lines, function (&$v) {
-//    $v = trim($v);
-//});
 
-$mainClass = sprintf("Year%s\Day%02s\Main", YEAR, DAY);
+$filename = sprintf("Day%02d/", DAY);
+$filename .= (TEST_MODE ? "test_" : "") . "input.txt";
+$lines = file($filename);
+array_walk($lines, function (&$v) {
+    $v = trim($v);
+});
 
-printf("\n--- %s, Day %s: %s ---\n\n", YEAR, DAY, $mainClass::title());
-printf("Answer 1: %s\n", $mainClass::TaskOne());
-printf("Answer 2: %s\n", $mainClass::TaskTwo());
+$mainClassname = sprintf("Day%02s\Main", DAY);
+
+$mainClass = new $mainClassname($lines);
+
+printf("\n--- Day %s: %s ---\n\n", DAY, $mainClass->title());
+printf("Answer 1: %s\n", $mainClass->one());
+printf("Answer 2: %s\n", $mainClass->two());
 
 printf(
     "\nRuntime = %f seconds\n\n",
---
+++ Year2022/Day01/Main.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Year2022\Day01;
+
+class Main
+{
+    public static function Title(): string
+    {
+        return "Calorie Counting";
+    }
+    
+    public static function TaskOne(): string
+    {
+        return "Task 1";
+    }
+    
+    public static function TaskTwo(): string
+    {
+        return "Task 2";
+    }
+}

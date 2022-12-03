6c8a32384f4461b6ee63fe5d52013fae6eb2f691
Paul Court <emails@paulcourt.co.uk>
2022-12-03 07:08:56 +0000

    Day 2

---
+++ Year2022/Day02/Main.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Year2022\Day02;
+
+class Main
+{
+    const LOSS_SCORE = 0;
+    const DRAW_SCORE = 3;
+    const WIN_SCORE = 6;
+ 
+    private static $shapeScores = [
+            'ROCK' => 1,
+            'PAPER' => 2,
+            'SCISSORS' => 3    
+    ];
+    
+    public static function Title(): string
+    {
+        return "Rock Paper Scissors";
+    }
+    
+    private static function cleanedLines(): array
+    {
+        $filename = __DIR__ . DIRECTORY_SEPARATOR . (TEST_MODE ? "test_" : "") . "input.txt";
+        $lines = file($filename);
+        array_walk($lines, function (&$v) {
+            $v = trim($v);
+        });
+        return $lines;
+    }
+    
+    private static function roundScore($them, $us): int
+    {
+        $score = match (true) {
+            ($them == $us) => self::DRAW_SCORE,
+            (($them == 'ROCK') && ($us == 'PAPER')) => self::WIN_SCORE ,
+            (($them == 'PAPER') && ($us == 'SCISSORS')) => self::WIN_SCORE,
+            (($them == 'SCISSORS') && ($us == 'ROCK')) => self::WIN_SCORE,
+            default => self::LOSS_SCORE
+        };
+        
+        $score += self::$shapeScores[$us];
+        
+        return $score;
+    }
+    
+    private static function shapeFor($them, $reqOutcome): string
+    {
+        if ($reqOutcome == 'DRAW') {
+            return $them;
+        }
+        
+        return match ($them . "_" . $reqOutcome) {
+            'ROCK_WIN' => 'PAPER',
+            'ROCK_LOSE' => 'SCISSORS',
+            'PAPER_WIN' => 'SCISSORS',
+            'PAPER_LOSE' => 'ROCK',
+            'SCISSORS_WIN' => 'ROCK',
+            'SCISSORS_LOSE' => 'PAPER'
+        };
+    }
+
+    public static function TaskOne(): string
+    {
+        $total = 0;
+        
+        $key = [
+            'A' => 'ROCK',
+            'B' => 'PAPER',
+            'C' => 'SCISSORS',
+            'X' => 'ROCK',
+            'Y' => 'PAPER',
+            'Z' => 'SCISSORS',
+        ];
+        
+        foreach (self::cleanedLines() as $line) {
+            list ($them, $us) = explode(' ', $line, 2);
+            $them = $key[$them];
+            $us = $key[$us];
+            
+            $total += self::roundScore($them, $us);
+        }
+        
+        return $total;
+    }
+    
+    public static function TaskTwo(): string
+    {
+        $total = 0;
+        
+        $themKey = [
+            'A' => 'ROCK',
+            'B' => 'PAPER',
+            'C' => 'SCISSORS'
+        ];
+        
+        $outcomeKey = [
+            'X' => 'LOSE',
+            'Y' => 'DRAW',
+            'Z' => 'WIN',
+        ];
+        
+        foreach (self::cleanedLines() as $line) {
+            list ($them, $outcome) = explode(' ', $line, 2);
+            $them = $themKey[$them];
+            $us = self::shapeFor($them, $outcomeKey[$outcome]);
+            
+            $total += self::roundScore($them, $us);
+        }
+        
+        return $total;
+    }
+}

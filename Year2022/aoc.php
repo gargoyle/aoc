9ba6c1e537cb8169619a5b0a452c7dd36baa6635
Paul Court <emails@paulcourt.co.uk>
2022-12-04 12:16:11 +0000

    Adds ability to run a single day and displays memory usage.

--- Year2022/aoc.php
+++ Year2022/aoc.php
@@ -17,11 +17,18 @@ define('START_TIME', microtime(true));
 
 $opts = getopt("t");
 define('TEST_MODE', isset($opts['t']));
-define('MAXDAY', $argv[TEST_MODE ? 2 : 1] ?? date('d'));
+$customDay = $argv[TEST_MODE ? 2 : 1] ?? false;
+if ($customDay) {
+    define('MAXDAY', $customDay);
+    define('MINDAY', $customDay);
+} else {
+    define('MAXDAY', date('d'));
+    define('MINDAY', 1);
+}
 define('YEAR', date('Y'));
 
 system('figlet AoC 2022');
-for ($d = 1; $d <= MAXDAY; $d++) {
+for ($d = MINDAY; $d <= MAXDAY; $d++) {
     $mainClass = sprintf("Year%s\Day%02s\Main", YEAR, $d);
 
     $title = sprintf("\nDay %s: %s", $d, $mainClass::title());
@@ -30,7 +37,12 @@ for ($d = 1; $d <= MAXDAY; $d++) {
     printf("\tPart Two: %s\n", $mainClass::TaskTwo());
 }
 
-printf(
-        "\nRuntime = %f seconds\n\n",
-        (microtime(true) - START_TIME)
-);
+$stats = [
+        'Memory (MB)' => memory_get_peak_usage(true) / 1048576.0,
+        'Runtime (s)' => (microtime(true) - START_TIME)
+];
+
+echo "\n\nStats\n-----\n";
+foreach ($stats as $k => $v) {
+    printf("% 16s: %1.4f\n", $k, $v);
+}

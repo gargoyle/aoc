67b5d74333a33834904c14287406bd9c63aed2ec
Paul Court <emails@paulcourt.co.uk>
2022-12-06 00:14:50 +0000

    Adds empty template for making each days code directory.

---
+++ Year2022/Empty-Day/Main.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Year2022\DayXX;
+
+class Main
+{
+    public static function Title(): string
+    {
+        return "Title Goes Here";
+    }
+
+    public static function TaskOne(): string
+    {
+        return "To-do...";
+    }
+
+    public static function TaskTwo(): string
+    {
+        return "To-do...";
+    }
+
+}
